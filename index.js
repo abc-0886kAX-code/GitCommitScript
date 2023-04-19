d4b8215f418e25e295d8176ca276630b16d05996
abc088_6kAX_code <86451477+abc-0886kAX-code@users.noreply.github.com>
2023-04-19 16:30:45 +0800

    :construction:
    :construction:

--- index.js
+++ index.js
@@ -3,19 +3,31 @@
  * @Author: zhangxin
  * @Date: 2023-04-19 13:04:20
  * @LastEditors: zhangxin
- * @LastEditTime: 2023-04-19 13:17:05
+ * @LastEditTime: 2023-04-19 15:29:24
  * @Description:
  */
 const fs = require('fs');
 const path = require('path');
+const child_process = require('child_process');
 const FileName = 'file.txt';
+const filePath = path.join(__dirname, FileName);
 const date = +new Date();
-
 // 写入文件
-function execute() {
-    fs.writeFile(path.join(__dirname, FileName), String(date), 'utf8', err => {
+function writeFile() {
+    fs.writeFile(filePath, String(date), 'utf8', err => {
         if (err) throw err;
     });
+}
+
+function submitTo() {
+    child_process.exec('git add .', { cwd: filePath }, function (error, stdout, stderr) {
+        console.log(error, stdout, stderr);
+    });
+}
+
+async function execute() {
+    await writeFile();
+    await submitTo();
 }
 
 execute();
